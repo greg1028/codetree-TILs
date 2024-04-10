1d5c689c62f7c6eb95351124dd9e4c10cfa30d11
Duk Hyun Yoo <greg1028@naver.com>
2024-04-11 00:25:28 +0900

    Title: 코드트리 빵, Time: 178ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240411/코드트리 빵/codetree-mon-bread.py
@@ -0,0 +1,96 @@
+from collections import deque
+N, M = map(int, input().split())
+root = [[True for _ in range(N)] for _ in range(N)] #True 면 이동 가능
+board = [list(map(int, input().split())) for _ in range(N)] #베이스 캠프 1
+con = tuple(tuple(map(lambda x:int(x) - 1, input().split())) for _ in range(M))
+people_loc = [(False, -1, -1) for _ in range(M)] #is_in, 좌표
+dx, dy = (-1, 0, 0, 1), (0, -1, 1, 0)
+
+def debug(board):
+    for i in range(N):
+        for j in range(N):
+            print(board[i][j], end=" ")
+        print()
+    print()
+
+def find_base_camp(i):
+    x, y = con[i]
+    q = deque()
+    q.append((x, y))
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    visited[x][y] = True
+    while q:
+        x, y = q.popleft()
+        for d in range(4):
+            nx, ny = dx[d] + x, dy[d] + y
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                visited[nx][ny] = True
+                if root[nx][ny]:
+                    if board[nx][ny] == 1: #베이스캠프라면
+                        board[nx][ny] = 0
+                        root[nx][ny] = False
+                        people_loc[i] = (True, nx, ny)
+                        return
+                    q.append((nx, ny))
+
+    raise "find_base_camp"
+
+def move(i):
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    _, sx, sy = people_loc[i]
+    q = deque()
+    q.append((sx, sy, []))
+    cx, cy = con[i]
+    find_way = False
+    while q:
+        x, y, path = q.popleft()
+        for d in range(4):
+            nx, ny = dx[d] + x, dy[d] + y
+            if 0 <= nx < N and 0 <= ny < N and root[nx][ny] and not visited[nx][ny]:
+                visited[nx][ny] = True
+                new = path + [d]
+                q.append((nx, ny, new))
+                if (nx, ny) == (cx, cy):
+                    find_way = True
+                    nd = new[0]
+                    mx, my = sx + dx[nd], sy + dy[nd]
+                    people_loc[i] = [True, mx, my]
+
+                    if (mx, my) == (cx, cy):
+                        people_loc[i][0] = False
+                        return True
+
+                if find_way:
+                    return False
+
+    return False
+
+remain = M #주의
+time = 0
+while remain > 0:
+
+    convi_reach_people = []
+    for i in range(M):
+        is_in, x, y = people_loc[i]
+        if not is_in: #나갔거나 아직 들어오지 않았다면
+            continue
+        reached = move(i)
+        if reached:
+            convi_reach_people.append(i)
+
+    #2. 편의점에 도착한 사람들 처리 ()
+    for i in convi_reach_people:
+        _, x, y = people_loc[i]
+        root[x][y] = False
+        remain -= 1
+
+    if remain == 0:
+        break
+
+    #3. t분의 사람 편의점과 가장 가까운 베이스 캠프에 들어감
+    if time < M:
+        find_base_camp(time)
+
+    time += 1
+
+print(time + 1)
